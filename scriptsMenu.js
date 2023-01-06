ef07bb47a3cb7ee45c742fb6d9299bcd09bc5a13
Bloxtermol10 <alefrito554@gmail.com>
2023-01-05 21:05:15 -0500

    Animaciones_completas_se_recomienda_optimizacion

--- scriptsMenu.js
+++ scriptsMenu.js
@@ -1,61 +1,96 @@
 // Abrir y cerrar modal
 let barsMenu = document.getElementById("barsMenu");
+const modal = document.createElement("div");
+const body = document.getElementById("root");
+const closeModal = document.createElement("img");
+const content = document.createElement("section");
 
 barsMenu.addEventListener("click", function () {
-  const modal = document.createElement("div");
-  const body = document.getElementById("root");
-  const closeModal = document.createElement("img");
-  const content = document.createElement("section");
-  let iconClose = "images/mobile/Close.svg";
-
   closeModal.classList.add("closeModal");
-  closeModal.setAttribute("src", iconClose);
+  closeModal.setAttribute("src", "images/mobile/Close.svg");
 
   content.classList.add("modal");
   content.appendChild(closeModal);
 
-
   modal.classList.add("wrapper-modal");
   modal.appendChild(content);
 
-
   body.appendChild(modal);
-  body.style.overflow ="hidden"
+  body.style.overflow = "hidden";
 
-  
   closeModal.addEventListener("click", function () {
-     modal.remove()
-     body.style.overflow ="scroll"
+    modal.remove();
+    body.style.overflow = "scroll";
   });
 });
 
-// SLIDER
+// Elementos DOM
 
-let slider = document.getElementById("slider")
-let wrapperSlider = document.getElementById("bebidas")
+let slider = document.getElementById("slider"),
+    wrapperSlider = document.getElementById("bebidas"),
+    box = document.getElementById("box"),
+    perro = document.getElementById("perro"),
+    dinamic1 = document.getElementById("dinamic1"),
+    elementDinamic1 = document.getElementById("elementDinamic1"),
+    up = document.getElementById("up");
+// Slider
+
+window.addEventListener("scroll", function () {
+
+  let position = wrapperSlider.getBoundingClientRect().top;
+  let pantalla = window.innerHeight / 2;
+  if (position < pantalla) {
+    slider.style.animation = "move 3s infinite";
+  }
+
+});
 
 
+// Perro
+perro.style.transform = "translateX(300px)";
 
-wrapperSlider.addEventListener('touchmove',function(){
-  slider.style.animation ="move 3s infinite"
-})
+window.addEventListener("scroll", function () {
 
-let box = document.getElementById("box")
-let perro = document.getElementById("perro")
-perro.style.transform="translateX(300px)"
-box.addEventListener('touchmove',function(){
-  perro.style.transform="translateX(0px)"
-}
-)
-
-let dinamic1 = document.getElementById("dinamic1");
-let elementDinamic1 =  document.getElementById("elementDinamic1");
+  let position = box.getBoundingClientRect().top;
+  let pantalla = window.innerHeight / 1.5;
+  if (position < pantalla) {
+    perro.style.transform = "translateX(0px)";
+  }else if(position < 0 || position<pantalla*3){
+    perro.style.transform = "translateX(300px)"
+  }
+});
 
 
-elementDinamic1.style.scale = "0"
-dinamic1.addEventListener('touchmove',()=>{
-  elementDinamic1.style.transition="scale, transform, 1s"
+// Arepa
+
+elementDinamic1.style.scale = "0";
+
+window.addEventListener("scroll", () => {
+
+  let position = dinamic1.getBoundingClientRect().top;
+  let pantalla = window.innerHeight / 2;
+
+  if (position < pantalla) {
+    elementDinamic1.style.transition = "scale, transform, 1s";
+    elementDinamic1.style.scale = "1";
+    elementDinamic1.style.transform = "rotate(360deg)";
+  }else if(position < 0 || position<pantalla*3){
+    elementDinamic1.style.scale = "0";
+    }
   
-  elementDinamic1.style.scale='1'
-  elementDinamic1.style.transform = "rotate(360deg)"  
+});
+
+// Up
+let positionAnterior = window.scrollY;
+window.addEventListener("scroll",()=>{
+  positionActual = window.scrollY   
+  if(positionAnterior<positionActual){
+    up.style.transform ="translateX(17vw)"
+    up.style.transition= "transform 0.5s"
+  }else if(positionAnterior>positionActual){
+    up.style.transform ="translateX(0)"
+    up.style.transition= "transform 0.5s"
+  }
+  positionAnterior = positionActual
 })
+
