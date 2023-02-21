45e72e05e30a51a351561a572d43c0dddf5ceeb6
Bloxtermol10 <alefrito554@gmail.com>
2023-02-20 20:54:04 -0500

    imagenes_jugos

--- Slider.js
+++ Slider.js
@@ -1,22 +1,31 @@
-const slider = document.querySelector('.slider');
-const slides = slider.querySelector('slide');
-const prevButton = slider.querySelector('#prevButton');
-const nextButton = slider.querySelector('#nextButton');
+const scr = (i) =>{
 
-let currentSlide = 0;
-const slideInterval = setInterval(nextSlide, 3000);
+  const contSlider = document.querySelector('.wrapper-jucie')
+  const slider = document.getElementsByClassName('slider');
+  const slides = slider[i].getElementsByClassName('slide');
+  const buttonsPrev = document.getElementById('prevButton') 
+  const buttonsNext = document.getElementById('nextButton') 
+  console.log(buttonsNext)
 
-function nextSlide() {
-  slides[currentSlide].style.opacity = 0;
-  currentSlide = (currentSlide + 1) % slides.length;
-  slides[currentSlide].style.opacity = 1;
+  const prevButton = buttonsPrev[i].querySelector('#prevButton');
+  const nextButton = buttonsNext[i].querySelector('#nextButton');
+  
+  let currentSlide = 0;
+  const slideInterval = setInterval(nextSlide, 3000);
+  function nextSlide() {
+    slides[currentSlide].style.opacity = 0;
+    currentSlide = (currentSlide + 1) % slides.length;
+    slides[currentSlide].style.opacity = 1;
+  }
+  
+  function prevSlide() {
+    slides[currentSlide].style.opacity = 0;
+    currentSlide = (currentSlide - 1 + slides.length) % slides.length;
+    slides[currentSlide].style.opacity = 1;
+  }
+  
+  prevButton.addEventListener('click', prevSlide);
+  nextButton.addEventListener('click', nextSlide);
 }
 
-function prevSlide() {
-  slides[currentSlide].style.opacity = 0;
-  currentSlide = (currentSlide - 1 + slides.length) % slides.length;
-  slides[currentSlide].style.opacity = 1;
-}
-
-prevButton.addEventListener('click', prevSlide);
-nextButton.addEventListener('click', nextSlide);
+scr(1)
