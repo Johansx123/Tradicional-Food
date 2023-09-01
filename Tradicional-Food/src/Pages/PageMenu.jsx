7901f87aaf9d4803fd1c2d3c227c83994988d27f
Diego Ramirez <alefrito554@gmail.com>
2023-08-31 20:29:28 -0500

    crate status an components for editing page

--- Tradicional-Food/src/Pages/PageMenu.jsx
+++ Tradicional-Food/src/Pages/PageMenu.jsx
@@ -5,13 +5,21 @@ import { Dinamic2 } from "../components/Products/Dinamic2";
 import { TableMenu } from "../components/Products/TableMenu";
 import responseProducts from '../mocks/products-menu1.json'
 import Beverage from "../components/Beverage";
+import { useState } from "react";
+import iconEdit from '../../public/edit.svg'
+import iconCancel from  '../../public/cancel.svg'
+import iconCheck from '../../public/check.svg'
 
-export default function PageMenu() {
+export default function PageMenu({isLogged}) {
+  const [isEditable, setIsEditable] = useState(false)
+
+
 
   const products = responseProducts?.Products
   
   return (
     <div className="background">
+      <Optios/>
       <main id="top" className="wrapper-main">
         <TitleMain />
         <IndexMenu/>
@@ -37,4 +45,25 @@ export default function PageMenu() {
       </main>
     </div>
   );
+}
+
+function Optios (){
+  const [isEditable, setIsEditable] = useState(false)
+  
+  const handleClick = () =>{
+    setIsEditable(!isEditable)
+  }
+
+  return(
+    <aside className="optionsEdit">
+      {
+        isEditable 
+        ?<>
+          <img src={iconCheck} alt="Guardar" />
+          <img src={iconCancel} alt="Cancelar" onClick={handleClick} />
+        </> 
+        : <img src={iconEdit} alt="Editar" onClick={handleClick}/>
+      }
+    </aside>
+  )
 }
