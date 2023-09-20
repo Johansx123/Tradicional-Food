d8eb90920a62a2ff5e7e30d5e2186acaa5683ffb
Diego Ramirez <alefrito554@gmail.com>
2023-09-19 19:48:11 -0500

    add product funcionality

--- src/components/Products/OverlayEdit.jsx
+++ src/components/Products/OverlayEdit.jsx
@@ -1,35 +1,46 @@
 import { InputForm } from "../atoms/InputForm";
 import { Button } from "../atoms/Button";
-import { updateProduct } from "../../js/Products";
+import { addArrayProducts, addProduct, updateProduct } from "../../js/Products";
+import { useEffect, useState } from "react";
 
 
 
 export function OverlayEdit({ id, name, description, price, setIsOpenOverlay }) {
 
+  const [categories , setCategories] = useState()
+  
+  const {handleUpdate, handleAdd} = useSendProducts({id})
+  
 
-  const handleSubmit = async (e) => {
-    const fields = Object.fromEntries(new window.FormData(e.target));
-    const name = fields?.name;
-    const description = fields?.description;
-    const price = fields?.price;
-    const data = {
-      name,
-      description,
-      price: parseFloat(price)
-    };
-    const message = await updateProduct(id, data);
-    console.log(message);
 
-  };
+  useEffect(()=>{
+    fetch ('https://api.tradicionalfood.com/categories')
+    .then(res => res.json())
+    .then(data => setCategories(data))
+  },[])
+ 
 
   const closeOverlay = () => {
     setIsOpenOverlay(false);
   };
+  
 
   return (
     <div className="overlayEdit">
-      <form style={{ padding: '2em', display: 'flex', flexDirection: 'column', gap: '1.5em', alignItems: 'center' }} onSubmit={handleSubmit}>
-        <InputForm name={'name'} label={"Nombre"} placeholder={name} type={'text'} key={1} />
+      <form style={{ padding: '2em', display: 'flex', flexDirection: 'column', gap: '1.5em', alignItems: 'center' }} onSubmit={handleAdd}>
+        <InputForm name={'name'} label={"Nombre"} placeholder={name} type={'text'}  />
+        <div className="fieldForm">
+        <label className='Font-small-15'htmlFor="category">Categoria</label>
+        <select className=" fieldForm-input" name="category" id="category">
+          <option value="-1">Seleccione una Categoria</option>
+          {categories
+            ? categories.map(e => {
+              return <option value={e.categoty_name} key={e.category_id}>{e.categoty_name}</option>
+            })
+            : null
+          }
+        </select>
+        </div>
         <InputForm name={'description'} label={"Descripción"} placeholder={description} type={'text'} />
         <InputForm name={'price'} label={"Precio"} placeholder={price} type={'number'} />
 
@@ -40,4 +51,44 @@ export function OverlayEdit({ id, name, description, price, setIsOpenOverlay }) 
 
       </form>
     </div>);
+}
+
+
+const useSendProducts = ({ id}) =>{
+
+  const handleUpdate = async (e) => {
+    const fields = Object.fromEntries(new window.FormData(e.target));
+    const name = fields?.name;
+    const description = fields?.description;
+    const price = fields?.price;
+    const category = fields?.category
+    const data = {
+      name,
+      category,
+      description,
+      price: parseFloat(price)
+    };
+    const message = await updateProduct(id, data);
+    console.log(message);
+
+  };
+
+  const handleAdd = async (e) =>{
+    e.preventDefault()
+    const fields = Object.fromEntries(new window.FormData(e.target));
+    const name = fields?.name;
+    const description = fields?.description;
+    const price = fields?.price;
+    const category = fields?.category;
+    const data = {
+      name,
+      description,
+      category :'Tradicional',
+      price: parseFloat(price)
+    };
+    const message = await addProduct(data);
+    console.log(message);
+  }
+
+  return {handleUpdate, handleAdd}
 }
